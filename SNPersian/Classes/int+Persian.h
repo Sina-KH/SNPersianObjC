77c65d1e3aa5d16d6ad7aa24defa72f6e2e46b81
SinaKH <sina_od@yahoo.com>
2017-07-14 01:55:04 +0430

    [upd] removed example project

---
+++ SNPersian/Classes/int+Persian.h
@@ -0,0 +1,15 @@
+//
+//  int+Persian.h
+//  SNPersian
+//
+//  Created by Sina Khalili on 7/14/17.
+//  Copyright © 2017 SinaKH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface int (SKPersian)
+
+-(NSString *) persianDigits;
+
+@end
---
+++ SNPersian/Classes/int+Persian.m
@@ -0,0 +1,14 @@
+//
+//  int+Persian.m
+//  SNPersian
+//
+//  Created by Sina Khalili on 7/14/17.
+//  Copyright © 2017 SinaKH. All rights reserved.
+//
+
+#ifndef int_persian_h
+#define int_persian_h
+
+#include <stdio.h>
+
+#endif /* int_persian_h */
